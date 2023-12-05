1927743adde5093dd2915b0c8aac15d5bb64ec1b
julesrx <dev@julesrx.fr>
2023-12-05 11:51:36 +0100

    feat: add spellcheck support

--- components/Editor.vue
+++ components/Editor.vue
@@ -7,6 +7,7 @@ const store = useStore();
         <textarea
             class="bg-transparent resize-none p-5 h-screen w-full"
             v-model="store.markup"
+            spellcheck
         ></textarea>
         <ModeSelection class="absolute bottom-0 right-0 m-2" />
     </div>
