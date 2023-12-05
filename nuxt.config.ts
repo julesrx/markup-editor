b67d1400b6a3bc54db355a86d3eff83ffdb0e987
julesrx <dev@julesrx.fr>
2023-12-05 10:52:30 +0100

    feat: force darkmode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
         baseURL,
         head: {
             title: 'Markup Editor',
-            htmlAttrs: { lang: 'en' },
+            htmlAttrs: { lang: 'en', class: 'dark' },
             bodyAttrs: { class: 'text-stone-200 bg-stone-950' },
             link: [{ rel: 'icon', type: 'image/svg+xml', href: `${baseURL}/favicon.svg` }]
         }
