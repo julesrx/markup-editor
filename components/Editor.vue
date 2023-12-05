a3841f39a89e75100e54dfd5a2a74fedf5453535
julesrx <dev@julesrx.fr>
2023-12-05 14:50:22 +0100

    feat: add tab control to editor

--- components/Editor.vue
+++ components/Editor.vue
@@ -1,5 +1,39 @@
 <script setup lang="ts">
 const store = useStore();
+
+const textarea = ref<HTMLTextAreaElement>();
+onMounted(() => {
+    if (!textarea.value) return;
+
+    const tab = '  ';
+
+    textarea.value.addEventListener('keydown', async e => {
+        const textarea = e.target as HTMLTextAreaElement;
+        if (e.code !== 'Tab') return;
+
+        e.preventDefault();
+
+        let newValue;
+        const selectionStart = textarea.selectionStart;
+        const currentValue = textarea.value;
+
+        if (currentValue) {
+            const valueBefore = textarea.value.substring(0, selectionStart);
+            const valueAfter = textarea.value.substring(selectionStart);
+
+            newValue = valueBefore + tab + valueAfter;
+        } else {
+            newValue = tab;
+        }
+
+        store.markup = newValue;
+
+        await nextTick();
+
+        textarea.selectionStart = selectionStart + tab.length;
+        textarea.selectionEnd = textarea.selectionStart;
+    });
+});
 </script>
 
 <template>
@@ -8,6 +42,7 @@ const store = useStore();
             class="bg-transparent resize-none p-5 w-full h-full"
             v-model="store.markup"
             spellcheck
+            ref="textarea"
         ></textarea>
         <ModeSelection class="absolute bottom-0 right-0 m-2" />
     </div>
